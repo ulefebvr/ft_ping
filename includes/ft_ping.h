1f70d2c0ff7eea2024d1e9c53adfecd63b04c195
ulefebvr <ulefebvr@student.42.fr>
2017-05-13 18:01:10 +0200

    [HEADER] Defined options

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 17:00:52 by ulefebvr          #+#    #+#             */
-/*   Updated: 2017/05/13 17:48:33 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2017/05/13 17:56:12 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define FT_PING_H
 
 # define OPTIONS		"vh"
+# define OPT_V			0x01
+# define OPT_H			0x02
 
 int		ft_ping(int options, char *destination);
 
--- srcs/ping.c
+++ srcs/ping.c
@@ -6,16 +6,18 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 17:49:00 by ulefebvr          #+#    #+#             */
-/*   Updated: 2017/05/13 17:49:17 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2017/05/13 17:59:57 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+#include "ft_ping.h"
+
 int					ft_ping(int options, char *destination)
 {
 	printf("%s\n", destination);
-	printf("v : %d\n", (options & 1) != 0);
-	printf("h : %d\n", (options & 2) != 0);
+	printf("v : %d\n", (options & OPT_V) != 0);
+	printf("h : %d\n", (options & OPT_H) != 0);
 	return (0);
 }
