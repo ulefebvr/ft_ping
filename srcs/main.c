ef529d96d7ac10f90dc1a2031da17c1c3c5461aa
ulefebvr <ulefebvr@student.42.fr>
2017-05-13 17:51:21 +0200

    [SRCS] Main init

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,51 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 17:00:46 by ulefebvr          #+#    #+#             */
-/*   Updated: 2017/05/13 17:04:41 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2017/05/13 17:49:06 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int			main(int ac, char **av)
+#include "libft.h"
+#include "option.h"
+#include "ft_ping.h"
+
+#include <stdio.h>
+
+void				usage(void)
 {
-	
+	dprintf(2, "%s\n", "./ft_ping [vh] destination");
+}
+
+int					get_option(int ac, char **av, int *opt)
+{
+	int				ch;
+	t_arguments		args;
+
+	ch = 0;
+	args = (t_arguments){ac, av, NULL};
+	while ((ch = option_getopt(&args, OPTIONS)) != -1)
+	{
+		if (ch == '?')
+		{
+			usage();
+			return (-1);
+		}
+		*opt |= 1 << (ft_strchr(OPTIONS, ch) - OPTIONS);
+	}
+	return (0);
+}
+
+int					main(int ac, char **av)
+{
+	int				opt;
+
+	opt = 0;
+	if (get_option(ac, av, &opt) == -1)
+		return (-1);
+	if ((ac - g_option_optind) != 1)
+	{
+		usage();
+		return (-1);
+	}
+	return (ft_ping(opt, *(av + g_option_optind)));
 }
