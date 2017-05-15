6303eb682f149fbca17d84212d657dd5362fa004
ulefebvr <ulefebvr@student.42.fr>
2017-05-15 10:16:34 +0200

    [SRCS] Main done

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 17:00:46 by ulefebvr          #+#    #+#             */
-/*   Updated: 2017/05/13 17:49:06 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2017/05/14 18:02:43 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "ft_ping.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void				usage(void)
 {
@@ -45,12 +46,17 @@ int					main(int ac, char **av)
 	int				opt;
 
 	opt = 0;
-	if (get_option(ac, av, &opt) == -1)
-		return (-1);
-	if ((ac - g_option_optind) != 1)
+	if (getuid() != 0)
+	{
+		dprintf(2, "%s\n", "Need root privilege to execute.");
+		return (EXIT_FAILURE);
+	}
+	if (ac < 2
+		|| (get_option(ac, av, &opt) == -1
+		|| (ac - g_option_optind) != 1))
 	{
 		usage();
-		return (-1);
+		return (EXIT_FAILURE);
 	}
 	return (ft_ping(opt, *(av + g_option_optind)));
 }
